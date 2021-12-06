a149d66446972c4309f90c80bcb8a27a2453f193
Hau Kool <haukool217@gmail.com>
2021-12-06 21:51:40 +0700

    khuyenmai

--- app/Http/Controllers/admin/KhuyenMaiController.php
+++ app/Http/Controllers/admin/KhuyenMaiController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\KhuyenMai;
+use App\Models\KhoaHoc;
 use Illuminate\Http\Request;
 
 class KhuyenMaiController extends Controller
 {
     public function index()
     {
-        return view('admin.khuyenmai.index');
+        $khuyenmai = KhuyenMai::all();
+        return view('admin.khuyenmai.index', ['khuyenmai'=>$khuyenmai]);
+    }
+
+    public function chitietkm()
+    {
+        $khoahoc = KhoaHoc::all();
+        return view('admin.khuyenmai.chitietkm', ['khoahoc'=>$khoahoc]);
     }
 }
